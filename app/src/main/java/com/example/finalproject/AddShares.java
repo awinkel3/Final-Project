bd5184d02718422c3d37e00c86506c399336c83d
Alex <awinkel3@illinois.edu>
2019-12-01 23:45:21 -0600

    Barebones UI up and running

--- app/src/main/java/com/example/finalproject/AddShares.java
+++ app/src/main/java/com/example/finalproject/AddShares.java
@@ -9,12 +9,22 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class AddShares extends AppCompatActivity {
+
+    //The total cost will be displayed and change as the input of number changes
+    //private TextView totalCost;
+
+    //The cost of each share, to be multiplied by amount when setting totalCost's text
+    private double shareCost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +36,55 @@ public class AddShares extends AppCompatActivity {
         //Gets activity's textViews and buttons
         TextView shareName = findViewById(R.id.shareName);
         TextView totalCost = findViewById(R.id.totalCost);
-        TextView shareCo = findViewById(R.id.shareCo);
+        TextView shareCompany = findViewById(R.id.shareCo);
         EditText shareNum = findViewById(R.id.shareNum);
-        Button complete = findViewById(R.id.completeButton);
+
+        shareCost = intent.getDoubleExtra("cost", 0);
 
         //Set shareName to stock's name
         shareName.setText(intent.getStringExtra("name"));
-        shareCo.setText(intent.getStringExtra("company"));
-        totalCost.setText("" + intent.getDoubleExtra("cost", 0.0));
+        shareCompany.setText(intent.getStringExtra("company"));
+        totalCost.setText("0");
+
+        //Tells program what to do when text changes in shareNum
+        TextWatcher costChange = new TextWatcher() {
+            boolean lengthZero;
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int before, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                lengthZero = charSequence.length() == 0;
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int newAmount;
+                if (lengthZero) {
+                    newAmount = 0;
+                } else {
+                    newAmount = Integer.parseInt(editable.toString());
+                }
+                totalCost.setText("" + (newAmount * shareCost));
+            }
+        };
+
+        shareNum.addTextChangedListener(costChange);
+
+        //Tells the program what to do when the enter key is pressed on the keyboard
+        TextView.OnEditorActionListener endActivity = new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                finish();
+                return true;
+            }
+        };
+
+        shareNum.setOnEditorActionListener(endActivity);
 
     }
+
 
 }
--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -30,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     //This is the currently selected stock
     private Stock selectedStock = null;
 
+    //Request codes for starting the addShare activity seeking a result
+    private int addShareAction = 0;
+    private int removeShareAction = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,11 +59,11 @@ public class MainActivity extends AppCompatActivity {
         modeGroup.setOnCheckedChangeListener((unused, checkedId) -> {
             // checkedId is the R.id constant of the currently checked RadioButton
             // Your code here: make only the selected mode's settings group visible
-            if (checkedId == R.id.marketView) {
+            if (checkedId == R.id.marketButton) {
                 marketView.setVisibility(android.view.View.VISIBLE);
                 portView.setVisibility(android.view.View.GONE);
 
-            } else if (checkedId == R.id.portView) {
+            } else if (checkedId == R.id.portButton) {
                 portView.setVisibility(android.view.View.VISIBLE);
                 marketView.setVisibility(android.view.View.GONE);
 
@@ -69,14 +73,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        //Populate marketView layout with the list of market stocks
+        //Populate marketStocks layout (contained within marketView) with the list of market stocks
+        LinearLayout marketStocks = findViewById(R.id.marketStocks);
         for (int i = 0; i < market.size(); i++) {
 
             //We're dealing with the stock at index(i) in market
             Stock currentStock = market.get(i);
 
             //Get the chunk and its three text elements to be set
-            View stockChunk = getLayoutInflater().inflate(R.layout.chunk_stock, marketView, false);
+            View stockChunk = getLayoutInflater().inflate(R.layout.chunk_stock, marketStocks, false);
             TextView stockName = stockChunk.findViewById(R.id.stockName);
             TextView stockCo = stockChunk.findViewById(R.id.stockCo);
             TextView stockCost = stockChunk.findViewById(R.id.cost);
@@ -91,7 +96,27 @@ public class MainActivity extends AppCompatActivity {
             //Makes it so clicking the buy button triggers the addShares method
             stockBuy.setOnClickListener(unused -> addShares(currentStock));
 
-            marketView.addView(stockChunk);
+            marketStocks.addView(stockChunk);
+        }
+
+        updatePortfolio();
+    }
+
+    /**
+     * Invoked by the Android system when a request launched by startActivityForResult completes.
+     * @param requestCode the request code passed by to startActivityForResult
+     * @param resultCode a value indicating how the request finished (e.g. completed or canceled)
+     * @param data an Intent containing results (e.g. as a URI or in extras)
+     */
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == addShareAction) {
+            // Do something that depends on the result of that request
+        }
+
+        if (requestCode == removeShareAction) {
+
         }
     }
 
@@ -108,8 +133,61 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("company", stock.getCompany());
         intent.putExtra("cost", stock.getCost());
 
-        startActivity(intent);
+        startActivityForResult(intent,addShareAction);
 
         //I don't think we finish so then the player can go back?
+    }
+
+
+    //Launches the addShares activity (but with the remove type so stocks are removed from the portfolio)
+    private void removeShares (Stock stock) {
+        Intent intent = new Intent(this, AddShares.class);
+
+        //I was an idiot and called the class AddShares, but it should be able to add or subtract shares
+        //The type should tell the program which one to do
+        intent.putExtra("type", "remove");
+
+        //Sends the info about the stock that will be used to set up the addShares screen
+        intent.putExtra("name", stock.getName());
+        intent.putExtra("company", stock.getCompany());
+        intent.putExtra("cost", stock.getCost());
+
+        startActivityForResult(intent,removeShareAction);
+    }
+
+    //Method for updating the user's portfolio
+    private void updatePortfolio() {
+        LinearLayout portStocks = findViewById(R.id.portStocks);
+        for (Map.Entry<Stock, Integer> entry : portfolio.entrySet()) {
+            // The type names in the angle brackets should match the types in the map
+            // The current key is entry.getKey()
+            // The current value is entry.getValue()
+            // Do something with the key and value?
+            //We're dealing with the stock at index(i) in market
+            Stock currentStock = entry.getKey();
+
+            //Get the chunk and its three text elements to be set
+            View stockChunk = getLayoutInflater().inflate(R.layout.chunk_portfolio_stock, portStocks, false);
+            TextView stockName = stockChunk.findViewById(R.id.stockName);
+            TextView stockCo = stockChunk.findViewById(R.id.stockCo);
+            TextView stockCost = stockChunk.findViewById(R.id.cost);
+            TextView stockNum = stockChunk.findViewById(R.id.number);
+            Button stockSell = stockChunk.findViewById(R.id.stockSell);
+
+            //Set three text elements to appropriate stock-related thingies
+            stockName.setText(currentStock.getName());
+            stockCo.setText(currentStock.getCompany());
+            //*It won't let me cast a double to a string so this is a work around
+            stockCost.setText("" + currentStock.getCost());
+
+            //Sets the number paramter to the number of the stock the user owns (the value of the list)
+            stockNum.setText("" + entry.getValue());
+
+            //Makes it so clicking the buy button triggers the addShares method
+            stockSell.setOnClickListener(unused -> removeShares(currentStock));
+
+            portStocks.addView(stockChunk);
+        }
+
     }
 }
